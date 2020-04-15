f0b2307d05723ff3accaa19666586a4185e6e614
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-15 19:09:42 +0800

    [Michelle] Complete PUT /employee/{employeeId}

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -46,4 +46,17 @@ public class EmployeeController {
 
         return new ResponseEntity<>(deleteEmployee, HttpStatus.OK);
     }
+
+    @PutMapping("/{employeeId}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int employeeId, @RequestBody Employee updateEmployee){
+        Employee employeeInList = employeeList.stream().filter(employee -> employee.getEmployeeId() == employeeId).findFirst().orElse(null);
+
+        if (employeeInList == null){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        employeeList.remove(employeeInList);
+        employeeList.add(updateEmployee);
+
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
 }
