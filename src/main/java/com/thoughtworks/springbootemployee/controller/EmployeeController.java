37258e368f775b18abf833f1757721d024f3654e
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-15 18:11:51 +0800

    [Michelle] modify the employeeList

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -5,25 +5,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    List<Employee> employeeList = new ArrayList<>();
+    public List<Employee> employeeList = new ArrayList<>(Arrays.asList(
+            new Employee(0, "Xiaoming", 20, "Male"),
+            new Employee(1, "Xiaohong", 19, "Female"),
+            new Employee(2, "Xiaozhi", 15, "Male"),
+            new Employee(3, "Xiaogang", 16, "Male"),
+            new Employee(4, "Xiaoxia", 15, "Female")
+    ));
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getEmployeeList(){
-        employeeList.add(new Employee(0, "Xiaoming", 20, "Male"));
-        employeeList.add(new Employee(1, "Xiaohong", 19, "Female"));
-        employeeList.add(new Employee(2, "Xiaozhi", 15, "Male"));
-        employeeList.add(new Employee(3, "Xiaogang", 16, "Male"));
-        employeeList.add(new Employee(4, "Xiaoxia", 15, "Female"));
+
         return employeeList;
     }
-
-    //(add, delete, change and search)
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
