a3634e676986bdaa30a78f6f094a899e23ea0bce
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-17 12:00:04 +0800

    [Michelle] Setup H2

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -1,60 +1,74 @@
 package com.thoughtworks.springbootemployee.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Employee {
-    public int employeeId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer employeeId;
     public String name;
     public Integer age;
     public String gender;
     public Integer salary;
 
-    public Employee() {
-    }
+//    public Employee() {
+//    }
+//
+//    public Employee(int employeeId, String name, Integer age, String gender, Integer salary) {
+//        this.employeeId = employeeId;
+//        this.name = name;
+//        this.age = age;
+//        this.gender = gender;
+//        this.salary = salary;
+//    }
 
-    public Employee(int employeeId, String name, Integer age, String gender, Integer salary) {
-        this.employeeId = employeeId;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.salary = salary;
-    }
-
-    public Integer getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+//    public Integer getEmployeeId() {
+//        return employeeId;
+//    }
+//
+//    public void setEmployeeId(int employeeId) {
+//        this.employeeId = employeeId;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+//
+//    public Integer getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(int salary) {
+//        this.salary = salary;
+//    }
 }
