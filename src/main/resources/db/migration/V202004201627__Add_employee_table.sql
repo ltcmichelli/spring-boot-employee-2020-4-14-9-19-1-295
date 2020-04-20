e721cc1cfb0133bcf8a432681a57a9958f011cb9
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-20 16:35:37 +0800

    [Michelle] Add_employee_table

---
+++ src/main/resources/db/migration/V202004201627__Add_employee_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS employee(
+    employee_id int auto_increment not null,
+    name varchar(30) not null,
+    age int not null,
+    gender varchar(30) not null,
+    salary int not null,
+    company_id int not null,
+    PRIMARY KEY(employee_id)
+)
