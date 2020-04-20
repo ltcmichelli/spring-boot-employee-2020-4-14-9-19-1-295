21a12991f543c3ccd12910ed08123c1bcdc43fa7
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-20 16:27:13 +0800

    [Michelle] Insert_company_record

--- src/main/resources/db/migration/V202004201330__Add_company_table.sql
+++ src/main/resources/db/migration/V202004201330__Add_company_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS company(
-    company_id int not null,
+    company_id int auto_increment not null,
     company_name varchar(30) not null,
     employees_number varchar(30) not null,
     PRIMARY KEY(company_id)
---
+++ src/main/resources/db/migration/V202004201620__Insert_company_record.sql
@@ -0,0 +1,2 @@
+INSERT INTO COMPANY (company_name, employees_number) VALUES ('Test 1', 100);
+INSERT INTO COMPANY (company_name, employees_number)  VALUES ('Test 2', 100);
